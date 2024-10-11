47b43c46a7fa87071f19790cff0a4ea82e64cb4d
uta1002092849 <mmt2849@mavs.uta.edu>
2024-10-11 16:19:28 -0500

    change naming

--- pages/_Ontology.py
+++ pages/_Ontology.py
@@ -4,98 +4,122 @@ from components.navigation_bar import navition_bar
 from st_link_analysis import st_link_analysis, NodeStyle, EdgeStyle
 from api.dao.general import GeneralDAO
 import random
-import re
 from st_link_analysis.component.layouts import LAYOUTS
-import collections
 
+# Constants
 LAYOUT_LIST = list(LAYOUTS.keys())
 CURVE_LIST = ["bezier", "haystack", "straight", "unbundled-bezier", "round-segments", "segments", "round-taxi", "taxi"]
 
-# Page config and icon
+# Page configuration
 st.set_page_config(layout="wide", page_title="Ontology View", page_icon=":satellite_antenna:")
 
+# Initialize Neo4j driver and DAO
 driver = init_driver()
 dao = GeneralDAO(driver)
 
-# Fetch ontology data
-elements = dao.get_ontology_data()
-
-# Function to generate random color
+# Helper functions
 def random_color():
+    """Generate a random color in hexadecimal format."""
     return "#{:06x}".format(random.randint(0, 0xFFFFFF))
 
-# Cache the elements
+def change_curve_style():
+    """Update the curve style for all edges."""
+    for edge in st.session_state.edge_styles:
+        edge.curve_style = st.session_state.curve_style
+
+def clear_cache():
+    """Clear all session state variables."""
+    for key in list(st.session_state.keys()):
+        del st.session_state[key]
+
+def update_elements():
+    """Update graph elements based on user interaction."""
+    selected_node = st.session_state.get("ontology", None)
+    if selected_node and selected_node["action"] == "expand":
+        node_id = selected_node["data"]['node_ids'][0]
+        raw_attributes, view_attributes = dao.get_node_attributes(node_id)
+        
+        for raw_attr, view_attr in zip(raw_attributes, view_attributes):
+            example_value = dao.get_example_value(node_id, raw_attr)
+            example_value = ', '.join(example_value) + ",..." if example_value else "N/A"
+            
+            new_node = {
+                "data": {
+                    "id": raw_attr,
+                    "label": raw_attr,
+                    "Example Values": example_value,
+                    "caption": view_attr
+                }
+            }
+            new_edge = {
+                "data": {
+                    "id": f"{node_id}-{raw_attr}",
+                    "label": "has_data_attribute",
+                    "caption": "has_data_attribute",
+                    "source": node_id,
+                    "target": raw_attr
+                }
+            }
+            
+            st.session_state.elements["nodes"].append(new_node)
+            st.session_state.elements["edges"].append(new_edge)
+            st.session_state.node_styles.append(NodeStyle(raw_attr, st.session_state.color[node_id], caption="caption"))
+
+# Initialize session state variables
 if "elements" not in st.session_state:
-    st.session_state.elements = elements
+    st.session_state.elements = dao.get_ontology_data()
 if "selected_node" not in st.session_state:
     st.session_state.selected_node = None
 if "color" not in st.session_state:
-    st.session_state.color = {}
-    for node in st.session_state.elements["nodes"]:
-        st.session_state.color[node["data"]["label"]] = random_color()
+    st.session_state.color = {node["data"]["id"]: random_color() for node in st.session_state.elements["nodes"]}
 if "node_styles" not in st.session_state:
-    st.session_state.node_styles = []
-    for node in st.session_state.elements["nodes"]:
-        st.session_state.node_styles.append(NodeStyle(node["data"]["label"], st.session_state.color[node["data"]["label"]], caption="label"))
+    st.session_state.node_styles = [NodeStyle(node["data"]["caption"], st.session_state.color[node["data"]["id"]], caption="caption") for node in st.session_state.elements["nodes"]]
 if "edge_styles" not in st.session_state:
-    st.session_state.edge_styles = []
-    st.session_state.edge_styles.append(EdgeStyle("has_data_attribute", caption="label", directed=True, curve_style="bezier"))
-    for edge in st.session_state.elements["edges"]:
-        st.session_state.edge_styles.append(EdgeStyle(edge["data"]["label"], caption="label", directed=True, curve_style="bezier"))
+    st.session_state.edge_styles = [EdgeStyle(edge["data"]["caption"], caption="caption", directed=True, curve_style="bezier") for edge in st.session_state.elements["edges"]]
+    st.session_state.edge_styles.insert(0, EdgeStyle("has_data_attribute", caption="caption", directed=True, curve_style="bezier"))
 
-# Sidebar for navigation
+# Sidebar navigation
 navition_bar()
 
+# Main content
 st.markdown("<h1 style='text-align: center; color: #4CAF50;'>Ontology View</h1>", unsafe_allow_html=True)
 
 # Layout configuration
-cols = st.columns(2)
-layout = cols[0].selectbox("Select Layout", LAYOUT_LIST, index=0)
-curve = cols[1].selectbox("Select Edge Style", CURVE_LIST)
+col1, col2, col3 = st.columns(3, vertical_alignment="bottom")
+with col1:
+    st.selectbox("Select Layout", LAYOUT_LIST, index=0, key="layout")
+with col2:
+    st.selectbox("Select Edge Style", CURVE_LIST, index=0, key="curve_style", on_change=change_curve_style)
+with col3:
+    st.button("Reset View", type="secondary", on_click=clear_cache, use_container_width=True)
 
-# Function to convert camel case to normal case
-def camel_to_normal(camel_str):
-    normal_str = re.sub(r'(?<!^)(?=[A-Z])', ' ', camel_str)
-    return normal_str.title()
-
-# Function to convert snake case to normal case
-def snake_to_normal(snake_str):
-    words = [word if word != 'per' else '/' for word in snake_str.split('_')]
-    return ' '.join(words)
-
-# Function to convert camel + snake case to normal case
-def camel_snake_to_normal(camel_snake_str):
-    underscore_index = camel_snake_str.find('_')
-    if underscore_index != -1:
-        normal_str = camel_to_normal(camel_snake_str[:underscore_index])
-        normal_str += ' (' + snake_to_normal(camel_snake_str[underscore_index + 1:]) + ')'
-        return normal_str
-    else:   
-        return camel_to_normal(camel_snake_str)
-
-# Update elements on node action
-def update_elements():
-    selected_node = st.session_state.get("ontology", None)
-    if selected_node:
-        action = selected_node["action"]
-        id = selected_node["data"]['node_ids'][0]
-        attributes = dao.get_node_attributes(id)
-        for i, attribute in enumerate(attributes):
-            attributes[i] = camel_snake_to_normal(attribute)
-
-        if action == "expand":
-            for attribute in attributes:
-                st.session_state.elements["nodes"].append({"data": {"id": attribute, "label": attribute}})
-                st.session_state.elements["edges"].append({"data": {"id": f"{id}-{attribute}", "label": "has_data_attribute", "source": id, "target": attribute}})
-                st.session_state.node_styles.append(NodeStyle(attribute, st.session_state.color[id], caption="label"))
-        elif action == "remove":
-            st.session_state.elements["nodes"] = [node for node in st.session_state.elements["nodes"] if node["data"]["id"] not in attributes]
-            st.session_state.elements["edges"] = [edge for edge in st.session_state.elements["edges"] if edge["data"]["source"] != id and edge["data"]["target"] not in attributes]
-
-            st.session_state.elements["nodes"].append({"data": {"id": id, "label": id}})
-
-
-# Display link analysis and pass the update function
+# Display graph
 st.session_state.selected_node = st_link_analysis(
-    st.session_state.elements, layout, st.session_state.node_styles, st.session_state.edge_styles, height=800, key="ontology", node_actions=['expand', 'remove'], on_change=update_elements
+    st.session_state.elements,
+    st.session_state.layout,
+    st.session_state.node_styles,
+    st.session_state.edge_styles,
+    height=800,
+    key="ontology",
+    node_actions=['expand'],
+    on_change=update_elements
 )
+
+# Add a section for user instructions
+with st.sidebar:
+    with st.expander("How to use this page", expanded=False):
+        st.write("## How to Use the Ontology Viewer")
+        
+        st.write("1️⃣ **Explore the Graph**: Click and drag to move around the canvas, and scroll to zoom in or out.")
+        
+        st.write("2️⃣ **Select Layout**: Choose a layout from the dropdown menu to reorganize the graph's structure.")
+        
+        st.write("3️⃣ **Edge Style**: Pick a different edge style from the dropdown to change the appearance of connections between nodes.")
+        
+        st.write("4️⃣ **Node Information**: Click on any node to view more detailed information about it.")
+        
+        st.write("5️⃣ **Expand Nodes**: Click on a node and select 'Expand' (or double click) to reveal its data properties.")
+        
+        st.write("6️⃣ **Reorganize Canvas**: Click the `Refresh Layout` button on the top right of the canvas to reorganize the graph based on recent changes.")
+        
+        st.write("7️⃣ **Reset View**: Use the `Reset View` button to collapse expanded nodes and return the graph to its initial state.")
